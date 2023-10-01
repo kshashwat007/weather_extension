82f1169254b944898c0e196993c653fd7c435a25
kshashwat007 <kshashwat007@gmail.com>
2023-10-01 14:12:11 +0530

    typescript interface and api setup

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import ReactDom from 'react-dom'
 import { createRoot } from 'react-dom/client'
 import './popup.css'
+import {fetchOpenWeatherData} from '../utils/api'
 
-const test = <img src='icon.png' />
+const App: React.FC<{}> = () => {
+
+  useEffect(() => {
+    fetchOpenWeatherData('Dubai')
+      .then((data) => console.log(data))
+      .catch((err) => console.log("Error while fetching data", err))
+  }, [])
+  
+  return (
+    <div>
+      <img src='icon.png'/>
+    </div>
+  )
+}
 
 
 const container = document.createElement('div')
 document.body.appendChild(container)
 const root = createRoot(container)
-root.render(test)
+root.render(<App />)
---
+++ src/utils/api.ts
@@ -0,0 +1,37 @@
+const OPEN_WEATHER_API_KEY = 'a1f2f5165e0e5447d9f312df5cc5528d'
+
+
+export interface OpenWeatherData {
+  name: string
+  main: {
+    feels_like: number,
+    humidity: number,
+    pressure: number,
+    temp: number,
+    temp_max: number,
+    temp_min: number
+  }
+  weather: {
+    description: string
+    icon: string
+    id: number
+    main: string
+  }[]
+  wind: {
+    deg: number
+    speed: number
+    gust: number
+  }
+}
+
+export async function fetchOpenWeatherData(city: string): Promise<OpenWeatherData> {
+  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${OPEN_WEATHER_API_KEY}`)
+
+  if (!res.ok) {
+    throw new Error('City not found')
+  }
+
+  const data: OpenWeatherData = await res.json()
+  
+  return data
+}
